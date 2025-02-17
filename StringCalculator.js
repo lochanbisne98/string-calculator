44203b36c89aa4cc22b79b594c6e1f5c1263a794
Lochan Bisne <lochan@98thpercentile.com>
2025-02-17 15:59:17 +0530

    test: empty string returns 0

---
+++ StringCalculator.js
@@ -0,0 +1,12 @@
+// stringCalculator.js
+class StringCalculator {
+    add(numbers) {
+        if (!numbers) {
+            return 0;
+        }
+
+        return 0;
+    }
+}
+
+module.exports = StringCalculator;
---
+++ StringCalculator.test.js
@@ -0,0 +1,21 @@
+// stringCalculator.test.js
+const StringCalculator = require('./stringCalculator');
+
+describe('StringCalculator', () => {
+    let calculator;
+
+    beforeEach(() => {
+        calculator = new StringCalculator();
+    });
+
+    describe('add', () => {
+        test('should return 0 for empty string', () => {
+            expect(calculator.add("")).toBe(0);
+        });
+
+        test('should return the number for a single number', () => {
+            expect(calculator.add("1")).toBe(1);
+            expect(calculator.add("5")).toBe(5);
+        });
+    });
+});
