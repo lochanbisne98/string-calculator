9b80a7ff7b795f62cf0646e6f23505953c05808b
Lochan Bisne <lochan@98thpercentile.com>
2025-02-17 16:15:33 +0530

    feat: implement custom delimiter support

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,11 +1,19 @@
-// stringCalculator.js
 class StringCalculator {
     add(numbers) {
         if (!numbers) {
             return 0;
         }
 
-        return numbers.split(/[,\n]/)
+        let delimiter = /[,\n]/;
+        let numbersToProcess = numbers;
+
+        if (numbers.startsWith('//')) {
+            const delimiterEnd = numbers.indexOf('\n');
+            delimiter = numbers[2];
+            numbersToProcess = numbers.substring(delimiterEnd + 1);
+        }
+
+        return numbersToProcess.split(delimiter)
             .map(num => parseInt(num))
             .reduce((sum, num) => sum + num, 0);
     }
